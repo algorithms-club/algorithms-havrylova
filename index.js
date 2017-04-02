e057720f81ca442f1c7cd36dde8e624591146bb3
Marina Gavrilova <mhavrylova@gmail.com>
2017-04-03 00:02:34 +0300

    added connect and isConnected functions

--- index.js
+++ index.js
@@ -2,10 +2,16 @@
 
 let bubbleSort = require('./bubble-sort');
 let coverByTile = require('./entry-test/coverByTile');
+let nextYearFind = require('./entry-test/nextYearFind');
+let watermelon = require('./entry-test/watermelon');
+let UnionFind = require ('./unionFind');
 
 let algolib = {
     bubbleSort,
-    coverByTile
+    coverByTile,
+    nextYearFind,
+    watermelon,
+    UnionFind
 }
 
 module.exports = algolib;

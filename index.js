55e57d2d8d9993f3250954a55f388404a11bd4b2
Marina Gavrilova <mhavrylova@gmail.com>
2017-04-05 22:39:52 +0300

    resolved conflicts

--- index.js
+++ index.js
@@ -2,12 +2,10 @@
 
 let bubbleSort = require('./bubble-sort');
 let coverByTile = require('./entry-test/coverByTile');
-let UnionFind = require ('./unionFind');
 
 let algolib = {
     bubbleSort,
-    coverByTile,
-    UnionFind
+    coverByTile
 }
 
 module.exports = algolib;

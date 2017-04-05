005fbacc3d2c00accf89a0457dff9d5f77eca6d9
Marina Gavrilova <mhavrylova@gmail.com>
2017-04-06 00:04:12 +0300

    fixed functions after code review

--- entry-test/nextYearFind.js
+++ entry-test/nextYearFind.js
@@ -1,30 +1,32 @@
 'use strict';
 
-	  
-
-function nextYearFind(year){
+function nextYearFind(year) {
 	let wrongNumber = 'Please enter a Number >= 1000 and <= 9000'; 
-	if ( year > 9000 || year < 1000 || year === null || year === undefined || isNaN( year ) ) {
+	
+  if ( year > 9000 || year < 1000 || isNaN( year ) ) {
 	  return (wrongNumber);
 	}
-  	let yearN = year.toString();
-    let nextYear = +year +1;
-    let nextYearString = nextYear.toString();
-    let year2 = nextYearString.split('');
-    let count = 0;
-      for( let i = 0; i < year2.length; i++ ){        	     
-                    for( let j = 0; j < year2.length; j++ ){
-                        if (j == i) continue;
-                        if (year2[i] === year2[j] ) {
-                            count++
-                            break
-                        } 
-                    }
-        }
-        if (count == 0) { 
-           return +year2.join(''); 
-        } else {
-          return nextYearFind(+year +1);
-        }    
+
+  let nextYear = +year +1;
+  let nextYearString = nextYear.toString();
+  let year2 = nextYearString.split('');
+  let count = 0;
+
+  for( let i = 0; i < year2.length; i++ ) {        	     
+      for( let j = 0; j < year2.length; j++ ) {
+          if (j == i) continue;
+          if (year2[i] === year2[j] ) {
+              count++;
+              break;
+          } 
+      }
+  }
+
+  if (count == 0) { 
+     return +year2.join(''); 
+  } else {
+    return nextYearFind(+year +1);
+  }    
 }
+
 module.exports = nextYearFind;
--- entry-test/watermelon.js
+++ entry-test/watermelon.js
@@ -1,18 +1,19 @@
 'use strict';
 
-function watermelon(weight){
+function watermelon(weight) {
    let wrongNumber = 'Please enter a Number >= 1 and <= 100';
    let canDivideEven = 'YES';
    let canNotDivideEven = 'NO';
 
-if ( weight < 1 || weight > 100 || weight === null || weight === undefined || isNaN( weight ) ) {
-  return ( wrongNumber );
-} else {
-    if (weight % 2 === 0) {
-      return ( canDivideEven );
-    } else {
-      return ( canNotDivideEven );
-    }
+	if ( weight < 1 || weight > 100 || isNaN( weight ) ) {
+	  return ( wrongNumber );
+	} else {
+	    if (weight % 2 === 0) {
+	      return ( canDivideEven );
+	    } else {
+	      return ( canNotDivideEven );
+	    }
+	}
 }
-}
+
 module.exports = watermelon;
